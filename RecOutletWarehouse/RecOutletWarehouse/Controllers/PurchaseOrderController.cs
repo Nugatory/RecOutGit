bbdad5425e1e73e2013f9f3dbd9525fc6d9f44be
Nugatory <nullandnugatory@gmail.com>
2013-10-24 13:09:50 -0600

    Initial commit 3

---
+++ RecOutletWarehouse/RecOutletWarehouse/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RecOutletWarehouse.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        //
+        // GET: /PurchaseOrder/
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult CreateNewPO() {
+
+            return View();
+        }
+
+        public ActionResult AddPOLineItem() {
+
+            return View();
+        }
+
+    }
+}
---
+++ RecOutletWarehouse/RecOutletWarehouse/Models/DataFetcherSetter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RecOutletWarehouse.Models
+{
+    public class DataFetcherSetter
+    {
+    }
+}
